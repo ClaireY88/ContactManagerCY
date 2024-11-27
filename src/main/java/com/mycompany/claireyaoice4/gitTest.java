f3b5120ee7df21800d8cbd970d15739e330cdad4
claireyao <yaocl@sheridancollege.ca>
2024-11-26 19:36:55 -0500

    fourth comment

---
+++ src/main/java/com/mycompany/claireyaoice4/gitTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.claireyaoice4;
+
+/**
+ *
+ * @author clair
+ */
+public class gitTest {
+    private int myProperty;
+
+    public int getMyProperty() {
+        return myProperty;
+    }
+
+    public void setMyProperty(int myProperty) {
+        this.myProperty = myProperty;
+    }
+}
